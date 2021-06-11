2bf8ffb0bee4a4ceb3313a84360176c202722e50
giacomounivr <giacomo.marchioro@univr.it>
2021-06-11 16:42:45 +0200

    Changed oreder set width and height

--- examples/0004-canvas-size.py
+++ examples/0004-canvas-size.py
@@ -1,6 +1,6 @@
 # implementation of Image and Canvas with Differing Dimensions https://iiif.io/api/cookbook/recipe/0004-canvas-size/
 from IIIFpres import iiifpapi3
-iiifpapi3.BASE_URL = r"xttps://iiif.io/api/cookbook/recipe/0004-canvas-size"
+iiifpapi3.BASE_URL = r"https://iiif.io/api/cookbook/recipe/0004-canvas-size"
 manifest = iiifpapi3.Manifest()
 manifest.set_id(extendbase_url="manifest.json")
 manifest.add_label("en","Still image from an opera performance at Indiana University")
@@ -13,8 +13,9 @@ annopage.set_id(extendbase_url=["page","p1","1"])
 annotation = annopage.add_annotation_toitems(targetid=canvas.id) 
 annotation.set_motivation("painting")
 annotation.set_id(extendbase_url=["annotation","p0001-image"])
-annotation.body.set_width(640)
 annotation.body.set_height(360)
-annotation.body.set_id("xttps://fixtures.iiif.io/video/indiana/donizetti-elixir/act1-thumbnail.png")
-annotation.body.set_format("Image")
+annotation.body.set_width(640)
+annotation.body.set_id("https://fixtures.iiif.io/video/indiana/donizetti-elixir/act1-thumbnail.png")
+annotation.body.set_format("image/jpg")
 annotation.body.set_type("Image")
+manifest.json_save("0004-canvas-size_manifest.json")
