5fdcd13e0679ea05916c7b568f7a43f4b0abe1ea
Giacomo Marchioro <giacomo.marchioro@univr.it>
2022-03-29 16:16:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setuptools.setup(
     version="0.1.1",
     author="Giacomo Marchioro",
     author_email="giacomomarchioro@outlook.com",
-    description="A tool creating JSON manifests compliant with IIIF presentation API 3.0",
+    description="A tool for easing the construction of JSON manifests compliant with IIIF API 3.0.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/giacomomarchioro/pyIIIFpres",
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
