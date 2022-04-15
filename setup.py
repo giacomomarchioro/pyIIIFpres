b163ed2c2ed4fcca0a47e51b67d16ffd29ba7566
giacomounivr <giacomo.marchioro@univr.it>
2022-04-15 14:32:55 +0200

    New version release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyIIIFpres",
-    version="0.1.1",
+    version="0.1.2",
     author="Giacomo Marchioro",
     author_email="giacomomarchioro@outlook.com",
     description="A tool for easing the construction of JSON manifests compliant with IIIF API 3.0.",
